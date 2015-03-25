9d57e5bcb068a819f7c1d38e5d42125b63581007
wdeng <wdeng@linkedin.com>
2015-03-24 21:27:36 -0700

    finished chapter 7 k-means and pca

---
+++ mlclass-ex7/findClosestCentroids.m
@@ -0,0 +1,44 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+
+% You need to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+%
+% Note: You can use a for-loop over the examples to compute this.
+%
+
+for ex = 1:size(idx,1)
+	example = X(ex, :);
+	min_dist = 1000000;
+	for i = 1:K
+		centroid = centroids(i, :);
+		dist = norm(example - centroid, 2);
+		if (dist < min_dist)
+			min_dist = dist;
+			idx(ex) = i;
+		end
+	end
+end
+
+
+
+
+
+% =============================================================
+
+end
+
